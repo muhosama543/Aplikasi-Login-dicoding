14e68487158921f131dbedeff49f57ac71602e25
Muh Osama <118024893+muhosama543@users.noreply.github.com>
2024-02-20 11:28:59 +0700

    Update login-script.js
    memberikan komentar

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -5,20 +5,27 @@
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
 
+/* Membuat Variabel view */
 const loginFormElement = document.querySelector('#loginForm');
 const inputEmailElement = document.querySelector('#inputEmail');
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+/* membuat variabel untuk email dan password */
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
+/* membuat Function email dan password dapat berjalan */
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
   const email = inputEmailElement.value;
   const password = inputPasswordElement.value;
+
+  /* jika email dan password yang dimasukkan benar maka akan menuju Home */
   if (email == expectedEmail && password == expectedPassword) {
     goToHome();
+
+    /* Jika email dan password yang dimasukkan salah, maka akan muncul pop up */
   } else {
     showPopUp();
   }
